be2546cd3f0dd97f5f66070c8148e7cca3dfc131
AshwiniMerugu <134482239+AshwiniMerugu@users.noreply.github.com>
2023-05-31 17:47:26 +0530

    1st coding class C#
    Operator and Loop logics and examples

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApp2
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            ////Console.WriteLine("Welcome to FSD course");
+//            //Console.Write("Welcome again");
+//            //Console.ReadLine(); //enter to exit
+//            ////Console.Read(); //enter to exit
+//            ////Console.ReadKey();  //any key to exit
+
+//            //Console.WriteLine("Enter first number:");
+
+//            //int num1 =Convert.ToInt32(Console.ReadLine());
+
+//            //Console.WriteLine("Enter Second number:");
+//            //int num2 = int.Parse(Console.ReadLine());
+
+//            //Console.WriteLine("Addition result is :"+(num1 + num2));
+//            //Console.ReadLine();
+
+//            //Console.WriteLine("Substraction result is :" + (num1 - num2));
+//            //Console.ReadLine();
+
+//            //Console.WriteLine("Multiplication result is :" + (num1 * num2));
+//            //Console.ReadLine();
+
+
+//            //Console.WriteLine("Enter your Name :");
+//           // String s1 = Console.ReadLine();
+//            //Console.WriteLine("My name is :" + s1);
+
+//            //implicit coversion
+//            int a = 1894738982;
+//            long b = a;
+//            Console.WriteLine("long b=" + b);
+
+//            //explicit conversion
+
+//            double d = 87162989.12637;
+//            Console.WriteLine("Current Value of d "+d);
+
+//            int x = (int)d;
+//            Console.WriteLine("x= "+x);
+
+
+//            Console.ReadKey();
+           
+            
+
+//        }
+//    }
+//}
